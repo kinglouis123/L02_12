6c99ba4073d71da738a414c73f266f6dc831327f
tagniam <jrmaingat@gmail.com>
2017-10-21 17:17:03 -0400

    Add database driver

---
+++ src/Stapp/app/src/main/java/com/stapp/database/DatabaseDriver.java
@@ -0,0 +1,33 @@
+package com.stapp.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+/**
+ * Created by jr on 21/10/17.
+ */
+
+public class DatabaseDriver extends SQLiteOpenHelper {
+
+  private static final int DATABASE_VERSION = 1;
+  private static final String DATABASE_NAME = "bank.db";
+
+  public DatabaseDriver(Context context) {
+    super(context, DATABASE_NAME, null, 1);
+
+  }
+
+  @Override
+  public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    sqLiteDatabase.execSQL("CREATE TABLE USER (ID INTEGER PRIMARY KEY NOT NULL)");
+  }
+
+  @Override
+  public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+    onCreate(sqLiteDatabase);
+  }
+}
