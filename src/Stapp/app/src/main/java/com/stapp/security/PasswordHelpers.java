c29011b62ec368ffe9c09e4f33edf80f0d70d187
tagniam <jrmaingat@gmail.com>
2017-10-21 18:34:29 -0400

    Implement password hashing helper

---
+++ src/Stapp/app/src/main/java/com/stapp/security/PasswordHelpers.java
@@ -0,0 +1,38 @@
+package com.stapp.security;
+
+import java.security.MessageDigest;
+
+/**
+ * Created by jr on 21/10/17.
+ */
+
+public class PasswordHelpers {
+  /**
+   * Returns a hashed version of password to be stored in database.
+   * @param password the unhashed password
+   * @return the hashsed password
+   */
+  public static String passwordHash(String password) {
+    MessageDigest md;
+    try {
+      md = MessageDigest.getInstance("SHA-256");
+      md.update(password.getBytes("UTF-8"));
+      byte[] digest = md.digest();
+      return String.format("%064x", new java.math.BigInteger(1, digest));
+
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  /**
+   * check if the database password matches user provided password.
+   * @param pw1 the password stored in the database.
+   * @param pw2 the user provided password (unhashed).
+   * @return true if passwords match, false otherwise.
+   */
+  public static boolean comparePassword(String pw1, String pw2) {
+    return pw1.equals(passwordHash(pw2));
+  }
+
+}
