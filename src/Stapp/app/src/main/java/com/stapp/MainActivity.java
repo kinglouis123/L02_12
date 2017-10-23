69520aaf7709d76c4d17926705528fc9f4057ef8
Rahul Kumar Saini <rahulkumar1998@hotmail.com>
2017-10-22 22:53:41 -0400

    Fixed bug where User would not log in when creating a new object when the user already existed

--- src/Stapp/app/src/main/java/com/stapp/MainActivity.java
+++ src/Stapp/app/src/main/java/com/stapp/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.stapp.database.InitializeDatabase;
-import com.stapp.databasehelpers.UserHelper;
+import com.stapp.users.Student;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +12,13 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     InitializeDatabase.initializeDatabase();
-    String roleName = "STUDENT";
-    int role1 = UserHelper.getRoleIdGivenRoleName(roleName);
-    Toaster.toastShort("ID of a " + roleName + " is: " + role1);
-    setContentView(R.layout.activity_main);
+    Student student = new Student("usernameforstudent", "student", "pass4student");
+
+      Toaster.toastShort(student.getName());
+      Toaster.toastShort(student.getPassword());
+      Toaster.toastShort(student.getRoleName());
+      Toaster.toastShort(student.getId() + "");
+      Toaster.toastShort(student.isLoggedIn() + "");
+
   }
 }
