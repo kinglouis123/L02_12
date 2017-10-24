38b837b0e6958ed9bcf63a667994a84a15ec7bb1
tagniam <jrmaingat@gmail.com>
2017-10-23 21:37:30 -0400

    Link register to login page

--- src/Stapp/app/src/main/java/com/stapp/MainActivity.java
+++ src/Stapp/app/src/main/java/com/stapp/MainActivity.java
@@ -1,12 +1,13 @@
 package com.stapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.stapp.activities.RegisterActivity;
 import com.stapp.database.InitializeDatabase;
 import com.stapp.databasehelpers.UserHelper;
 import com.stapp.terminals.LoginTerminal;
@@ -21,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
   Professor user2 = new Professor("shierry", "Shierry Tans", "shierry");
   Student user3 = new Student("john", "John Doe", "john");
   Professor user4 = new Professor("Kohee", "Kohee Sang", "kohee");
+
+  protected void showRegisterActivity(View view) {
+    Intent intent = new Intent(this, RegisterActivity.class);
+    startActivity(intent);
+  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
---
+++ src/Stapp/app/src/main/java/com/stapp/activities/RegisterActivity.java
@@ -0,0 +1,19 @@
+package com.stapp.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.stapp.R;
+
+/**
+ * Created by jr on 23/10/17.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_register);
+
+  }
+}
