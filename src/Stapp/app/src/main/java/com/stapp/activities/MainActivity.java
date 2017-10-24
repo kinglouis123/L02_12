b7055f935a52cf1c083a96cebbfee1c22f36fa3b
Rahul Kumar Saini <rahulkumar1998@hotmail.com>
2017-10-23 22:03:53 -0400

    onBackPressed in main activity now goes to android home instead of previous activity

--- src/Stapp/app/src/main/java/com/stapp/activities/MainActivity.java
+++ src/Stapp/app/src/main/java/com/stapp/activities/MainActivity.java
@@ -60,4 +60,13 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_log_in);
 
   }
+
+  @Override
+  public void onBackPressed() {
+    Intent startMain = new Intent(Intent.ACTION_MAIN);
+    startMain.addCategory(Intent.CATEGORY_HOME);
+    startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    startActivity(startMain);
+  }
+
 }
