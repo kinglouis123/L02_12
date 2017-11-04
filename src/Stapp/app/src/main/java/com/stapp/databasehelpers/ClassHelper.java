95b58c082cd56d83fe6a0d23cad4b327ac7e70be
J.R. Maingat <jrmaingat@gmail.com>
2017-11-04 19:48:00 -0400

    Implement ClassHelper methods

--- src/Stapp/app/src/main/java/com/stapp/databasehelpers/ClassHelper.java
+++ src/Stapp/app/src/main/java/com/stapp/databasehelpers/ClassHelper.java
@@ -3,6 +3,8 @@ package com.stapp.databasehelpers;
 import com.stapp.database.ContextHelper;
 import com.stapp.database.DatabaseDriver;
 
+import java.util.ArrayList;
+
 /**
  * Created by JR on 2017-11-01.
  */
@@ -29,6 +31,59 @@ public class ClassHelper {
 
     public static long insertStudentToClass(String className, String studentUsername) {
         openDatabase();
-        return databaseDriver.insertStudentToClass(className, studentUsername);
+        long id = databaseDriver.insertStudentToClass(className, studentUsername);
+        closeDatabase();
+        return id;
     }
+
+    public static boolean archiveClass(String className) {
+        openDatabase();
+        boolean updated = databaseDriver.archiveClass(className);
+        closeDatabase();
+        return updated;
+    }
+
+    public static boolean removeStudentFromClass(String className, String username) {
+        openDatabase();
+        boolean updated = databaseDriver.removeStudentFromClass(className, username);
+        closeDatabase();
+        return updated;
+    }
+
+    public ArrayList<String> getStudentUsernames(String className) {
+        openDatabase();
+        ArrayList<String> usernames = databaseDriver.getStudentUsernames(className);
+        closeDatabase();
+        return usernames;
+    }
+
+    public ArrayList<String> getStudentClassNames(String username) {
+        openDatabase();
+        ArrayList<String> classNames = databaseDriver.getStudentClassNames(username);
+        closeDatabase();
+        return classNames;
+    }
+
+    public String getProfUsername(String className) {
+        openDatabase();
+        String profName = databaseDriver.getProfUsername(className);
+        closeDatabase();
+        return profName;
+    }
+
+    public boolean classNotArchived(String className) {
+        openDatabase();
+        boolean notArchived = databaseDriver.classNotArchived(className);
+        closeDatabase();
+        return notArchived;
+    }
+
+    public boolean classExists(String className) {
+        openDatabase();
+        boolean classExists = databaseDriver.classExists(className);
+        closeDatabase();
+        return classExists;
+    }
+
+
 }
