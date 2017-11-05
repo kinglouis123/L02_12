152325a4d66d7ac07100d234dbeb59cab0941610
J.R. Maingat <jrmaingat@gmail.com>
2017-11-04 22:14:46 -0400

    Complete course creation backend

--- src/Stapp/app/src/main/java/com/stapp/activities/CreateClass.java
+++ src/Stapp/app/src/main/java/com/stapp/activities/CreateClass.java
@@ -1,23 +1,43 @@
 package com.stapp.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.stapp.R;
+import com.stapp.Toaster;
+import com.stapp.terminals.CourseTerminal;
 
 public class CreateClass extends AppCompatActivity {
+    private String profUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Saves professor username for class generation later
+        Intent intent = getIntent();
+        this.profUsername = intent.getStringExtra("username");
+
         setContentView(R.layout.activity_create_class);
     }
 
     protected void createClass(View view) {
         // Call class helper to register here
-        // TODO
+        EditText courseCode = (EditText) findViewById(R.id.CreateClassName);
+        if (courseCode.getText() == null || courseCode.getText().equals("")) {
+            Toaster.toastShort("Invalid course code!");
+        }
 
-        // Output unique key after class creation
+        // Attempt to create course
+        if (CourseTerminal.createNewCourse(courseCode.getText().toString(),
+                this.profUsername) != null) {
+            Toaster.toastLong("Course successfully created!");
+            finish();
+        } else {
+            Toaster.toastLong("Course creation unsuccessful.");
+            finish();
+        }
     }
 }
