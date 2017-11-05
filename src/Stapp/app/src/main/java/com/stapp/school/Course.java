5fd0d8623ef3060d6fcad6df6aed3d61229a1183
Rahul Kumar Saini <rahulkumar1998@hotmail.com>
2017-11-04 21:16:24 -0400

    Moved classes.java to another package and reformatted it to Course.java

---
+++ src/Stapp/app/src/main/java/com/stapp/school/Course.java
@@ -0,0 +1,49 @@
+package com.stapp.school;
+
+import java.util.ArrayList;
+
+import com.stapp.databasehelpers.ClassHelper;
+import com.stapp.exceptions.StudentAlreadyExistsException;
+import com.stapp.exceptions.UserNotFoundException;
+
+
+/**
+ * Created by wenboma on 04/11/2017.
+ */
+
+public class Course {
+
+  private String courseCode;
+
+  public Course(String courseCode) {
+    this.courseCode = courseCode;
+  }
+
+  public String getProf() {
+    try {
+      return ClassHelper.getProfUsername(courseCode);
+    } catch(ClassNotFoundException e) {
+      return null;
+    }
+  }
+
+  public void addStudent(String username) throws StudentAlreadyExistsException, ClassNotFoundException {
+    ClassHelper.insertStudentToClass(courseCode, username);
+  }
+
+  public ArrayList<String> getStudentUsernames() {
+    try  {
+      ArrayList<String> studentlist = ClassHelper.getStudentUsernames(courseCode);
+      return studentlist;
+    } catch (ClassNotFoundException E) {
+      return null;
+    }
+  }
+
+  public void removeStudent(String username) {
+    try {
+      ClassHelper.removeStudentFromClass(courseCode, username);
+    } catch (UserNotFoundException e) {
+    }
+  }
+}
