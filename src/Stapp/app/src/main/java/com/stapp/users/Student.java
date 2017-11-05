73813728dca228aaeb9afcea34a03aafe9cac3fb
J.R. Maingat <jrmaingat@gmail.com>
2017-11-04 21:44:57 -0400

    Make Students get a list of their Course objects

--- src/Stapp/app/src/main/java/com/stapp/users/Student.java
+++ src/Stapp/app/src/main/java/com/stapp/users/Student.java
@@ -1,7 +1,13 @@
 package com.stapp.users;
 
+import com.stapp.databasehelpers.ClassHelper;
 import com.stapp.databasehelpers.UserHelper;
 import com.stapp.exceptions.UserNotFoundException;
+import com.stapp.school.Course;
+import com.stapp.terminals.CourseTerminal;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by rahulkumar1998 on 2017-10-22.
@@ -15,6 +21,25 @@ public class Student extends User {
 
   public Student(String username, String name, String password) {
     super(username, name, password, UserHelper.getRoleIdGivenRoleName("STUDENT"));
+  }
+
+  public List<Course> getCourses() {
+    try {
+      List<String> courseCodes = ClassHelper.getStudentClassNames(this.getUsername());
+      List<Course> courses = new ArrayList<>();
+
+      for (String code : courseCodes) {
+        Course curr = CourseTerminal.getCourse(code);
+        if (curr != null) {
+          courses.add(curr);
+        }
+      }
+
+      return courses;
+    } catch (UserNotFoundException e) {
+      // Should be impossible since Student object already exists
+      return null;
+    }
   }
 
 }
