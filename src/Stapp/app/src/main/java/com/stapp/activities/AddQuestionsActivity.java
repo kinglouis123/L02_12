e349feaf91baa3d79b5597d4a113725621e5f95c
harry1029 <harry10291993@hotmail.com>
2017-11-18 15:57:07 -0500

    Modified Assignment Questions
    Fixed the bug and modified the function of add assignment questions

---
+++ src/Stapp/app/src/main/java/com/stapp/activities/AddQuestionsActivity.java
@@ -0,0 +1,15 @@
+package com.stapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.stapp.R;
+
+public class AddQuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_questions);
+    }
+}
--- src/Stapp/app/src/main/java/com/stapp/activities/CreateNewAssignment.java
+++ src/Stapp/app/src/main/java/com/stapp/activities/CreateNewAssignment.java
@@ -1,50 +1,54 @@
 package com.stapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import com.stapp.school.Assignment; 
+
+import com.stapp.databasehelpers.AssignmentHelper;
+import com.stapp.school.Assignment;
 import com.stapp.terminals.AssignmentTerminal;
 
 import com.stapp.R;
+import android.widget.EditText;
 
 public class CreateNewAssignment extends AppCompatActivity {
     private String course_code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_new_assignment);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_new_assignment);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
 
-	Intent intent = getIntent(); 
-	course_code = intent.getStringExtra("course_code");
+		Intent intent = getIntent();
+		course_code = intent.getStringExtra("course_code");
 
-       
-        });
+
+	}
 
     protected void showAddQuestions(View view){
-	Intent intent = new Intent(this, AddQuestions.class);
+	Intent intent = new Intent(this, AddQuestionsActivity.class);
 
 	//get the assignment info from the edit texts
-	private String year;
-	private String month;
-	private String day;
-	private String name;
-	private Assignment assignment;
+	String year;
+	String month;
+	String day;
+	String name;
+	Assignment assignment;
 	
-	name = findViewById(R.id.CreateAssignmentName).getText().toString();
-	year = findViewById(R.id.CreateAssignmentDueDateYear).getText().toString();
-	month = findViewById(R.id.CreateAssignmentDueDateMonth).getText().toString(); 
-	day = findViewById(R.id.CreateAssignmentDueDateDay).getText().toString(); 
+	name = ((EditText)findViewById(R.id.CreateAssignmentName)).getText().toString();
+	year = ((EditText)findViewById(R.id.CreateAssignmentDueDateYear)).getText().toString();
+	month = ((EditText)findViewById(R.id.CreateAssignmentDueDateMonth)).getText().toString();
+	day = ((EditText)findViewById(R.id.CreateAssignmentDueDateDay)).getText().toString();
 
 	//create the duedate string format "yyyy-mm-dd"
 	
-	private String duedate;
+	String duedate;
 	duedate = year.substring(0, 3)+"-"+month.substring(0,1)+"-"+day.substring(0,1);
 
 	//create the new Assignment
