18876750e5a0413065a8424006bb112377439be2
Rahul Kumar Saini <rahulkumar1998@hotmail.com>
2017-11-20 16:50:52 -0500

    Finished professor tests

---
+++ src/Stapp/app/src/androidTest/java/com/stapp/users/ProfessorTest.java
@@ -0,0 +1,62 @@
+package com.stapp.users;
+
+import com.stapp.database.DatabaseDriverHelper;
+import com.stapp.database.InitializeDatabase;
+import com.stapp.school.Course;
+import com.stapp.terminals.CourseTerminal;
+import com.stapp.terminals.LoginTerminal;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by rahulkumar1998 on 2017-11-20.
+ */
+public class ProfessorTest {
+
+  @Before
+  public void setUp() throws Exception {
+    InitializeDatabase.initializeDatabase();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    DatabaseDriverHelper.reinitializeDatabase();
+  }
+
+  /**
+   * Random string generator.
+   * obtained from: https://stackoverflow.com/questions/20536566/creating-a-random-string-with-a-z-and-0-9-in-java
+   */
+  private String generateRandomString() {
+    String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    StringBuilder salt = new StringBuilder();
+    Random rnd = new Random();
+    while (salt.length() < 18) { // length of the random string.
+      int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+      salt.append(SALTCHARS.charAt(index));
+    }
+    return salt.toString();
+  }
+
+  @Test
+  public void getCourses() throws Exception {
+    String username = generateRandomString();
+    String course1 = generateRandomString();
+    String course2 = generateRandomString();
+    Professor prof = LoginTerminal.newProfessor(username, generateRandomString(), generateRandomString());
+    CourseTerminal.createNewCourse(course1, username);
+    CourseTerminal.createNewCourse(course2, username);
+
+    ArrayList<Course> courses = (ArrayList<Course>) prof.getCourses();
+
+    assertEquals(2, courses.size());
+  }
+
+}
